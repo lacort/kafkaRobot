cbdb06af7562b0aa7f841c0fea71cf376d2fefd0
lacort <lacort2@gmail.com>
2022-09-13 13:09:55 -0300

    Update

--- config/main.js
+++ config/main.js
@@ -2,10 +2,11 @@ module.exports = {
 
   // MONGO DB CONFIG
   kafka: {
-    strConnection: 'String',
+    strConnection: 'mongodb+srv://aiven:OVh4U5LH87m39D21@mdb-reports-sandbox-dea9eba9.mongo.ondigitalocean.com/aiven?tls=true&authSource=admin&replicaSet=mdb-reports-sandbox',
     options: {
       keepAlive: true,
-      keepAliveInitialDelay: 300000,
+      keepAliveInitialDelay: 300000
      
     }
   }
+}
